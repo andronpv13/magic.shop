c1d76fd1e8c5de25bfb93cf0ff948fcda783b16e
andronpv13 <92641203+andronpv13@users.noreply.github.com>
2026-05-12 19:32:41 +0600

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -1,8 +1,6 @@
 <?php
 $page_title = 'Оформление заказа';
-require_once __DIR__ . '/includes/config.php';
 require_once __DIR__ . '/includes/header.php';
-require_once __DIR__ . '/includes/functions.php';
 
 if (!isLoggedIn()) { header('Location: /login.php'); exit; }
 
