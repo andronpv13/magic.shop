9c3e9bd1ecedc280414b57562f9dadd1417336f7
andronpv13 <92641203+andronpv13@users.noreply.github.com>
2026-05-12 18:13:58 +0600

    Update profile.php

--- users/profile.php
+++ users/profile.php
@@ -55,7 +55,7 @@ if (!$user) {
                 </div>
                 <div class="info-row">
                     <span class="info-label">Отчество:</span>
-                    <span class="info-value"><?php echo e($user['patronymic'] ?? ''); ?></span>
+                    <span class="info-value"><?php echo e($user['middle_name'] ?? ''); ?></span>
                 </div>
                 <div class="info-row">
                     <span class="info-label">Телефон:</span>
@@ -63,7 +63,16 @@ if (!$user) {
                 </div>
                 <div class="info-row">
                     <span class="info-label">Адрес:</span>
-                    <span class="info-value"><?php echo e($user['address'] ?? ''); ?></span>
+                    <span class="info-value"><?php
+                        $address_parts = array_filter([
+                            $user['region'] ?? '',
+                            $user['city'] ?? '',
+                            $user['street'] ?? '',
+                            $user['house'] ?? '',
+                            $user['apartment'] ?? ''
+                        ]);
+                        echo e(implode(' ', $address_parts));
+                    ?></span>
                 </div>
             </div>
             <a href="edit_profile.php" class="btn btn-primary">Редактировать данные</a>
