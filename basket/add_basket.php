ceed40372d370e5f68cdc5ec81a0630b10c17eb8
andronpv13 <92641203+andronpv13@users.noreply.github.com>
2026-05-12 22:32:22 +0600

    Add configuration and functions includes to add_basket.php

--- basket/add_basket.php
+++ basket/add_basket.php
@@ -1,4 +1,6 @@
 <?php
+require_once __DIR__ . '/../includes/config.php';
+require_once __DIR__ . '/../includes/functions.php';
 require_once __DIR__ . '/../includes/header.php';
 header('Content-Type: application/json');
 
